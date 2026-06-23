c50a13d2ab54e2950cec911ac3080a226a60fe37
Javascriptdev347 <javascriptdev347@gmail.com>
2026-06-23 09:16:53 +0500

    BRR. day 2 section 1 variable

--- week_1/day_2/day_2.md
+++ week_1/day_2/day_2.md
@@ -1,1 +1,10 @@
 # Day 2
+# JavaScript nima ?
+
+# O’zgaruvchilarning turlari va farqlari(const, let, var) 
+
+# Execution context nima ?
+
+# TDZ (Temporal Dead Zone) nima ?
+
+# Hoisting nima ?
--- week_1/day_2/main.js
+++ week_1/day_2/main.js
@@ -1,1 +1,25 @@
-// main
+// variable
+
+// var
+// U global yoki funksional skop bilan ishlaydi.
+// Global e'lon qilinsa window obyektiga yoziladi (let va const unday emas).
+// bir xil nom bilan qayta e'lon qilish mumkin.
+// hoisting ishlaydi va undefined qiymat beradi.
+var a = 10;
+var a = "Salom";
+// console.log(window.a); // Salom
+
+// let
+// Block scope bilan ishlaydi.
+// Bir xil nom bilan qayta e'lon qilish mumkin emas xatolik beradi.
+// hoistingda ishlamaydi va xatolik beradi.
+let b = 20;
+// let b = "salom" // error
+//
+// const
+// Block scope bilan ishlaydi.
+// Bir xil nom bilan qayta e'lon qilish mumkin emas xatolik beradi.
+// lekin obyekt va massivlarning ichki qiymatlarini o’zgartirish mumkin
+// hoistingda ishlamaydi va xatolik beradi.
+const c = 30;
+// const c = "salom" // error
